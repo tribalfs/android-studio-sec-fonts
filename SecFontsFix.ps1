3177e4fe9845e2e24e68e29012da9b49d8f0656d
Tribalfs <tribalfs@gmail.com>
2025-01-10 23:40:21 +0800

    Support latest android studio

--- SecFontsFix.ps1
+++ SecFontsFix.ps1
@@ -41,6 +41,7 @@ function Merge-XmlContents {
 
 # Function to run Script 1
 function Run-SecFontsFix {
+	Write-Host "`n`n`n"
     Write-Host "Start Sec Fonts Render Fix..."
     $secFontsPath = "$PSScriptRoot\secfonts"
     Set-Location "$env:ProgramFiles\Android"
@@ -51,6 +52,7 @@ function Run-SecFontsFix {
         $dir = $_.FullName
         $fontsDirectory = "$dir\plugins\design-tools\resources\layoutlib\data\fonts"
         if (Test-Path $fontsDirectory) {
+			Write-Host "Working on $fontsDirectory..."
             $found = $true
             $fontsBackupDirectory = "$dir\plugins\design-tools\resources\layoutlib\data\fonts_backup"
             if (!(Test-Path $fontsBackupDirectory)) {
@@ -61,24 +63,48 @@ function Run-SecFontsFix {
             }
             Write-Host "Copying sec font ttf files..."
             robocopy $secFontsPath $fontsDirectory *.ttf /IS /IM /XC /XN /njh /njs /ndl /nc /ns /np /nfl /ndl
-       
-            # Merge contents of fonts.xml with patch
-            Write-Host "Merging sec fonts.xml..."
+            Write-Host "... done!"
+            Write-Host ""
+			# Merge contents of fonts.xml with patch to AS fonts.xml
             $fontsXmlPath = Join-Path $fontsDirectory "fonts.xml"
-            $patchFilePath = Join-Path $secFontsPath "fonts.xml"
-            $mainXml = [xml](Get-Content $fontsXmlPath)
-            $patchXml = [xml](Get-Content $patchFilePath)
-            Merge-XmlContents -mainXml $mainXml -patchXml $patchXml
-            $mainXml.Save($fontsXmlPath)
+			if (Test-Path $fontsXmlPath) {
+				Write-Host "Merging sec fonts.xml to android studio fonts.xml..."
+				$patchFilePath = Join-Path $secFontsPath "fonts.xml"
+				$mainXml = [xml](Get-Content $fontsXmlPath)
+				$patchXml = [xml](Get-Content $patchFilePath)
+				Merge-XmlContents -mainXml $mainXml -patchXml $patchXml
+				$mainXml.Save($fontsXmlPath)
+				Write-Host "... done!"
+			} else {
+				Write-Host "fonts.xml not found in android studio fonts directory!"
+			}
+			Write-Host ""
+			
+			# Merge contents of fonts.xml with patch to AS font_fallback.xml
+            
+            $fontFallbackXmlPath = Join-Path $fontsDirectory "font_fallback.xml"
+			if (Test-Path $fontFallbackXmlPath) {
+				Write-Host "Merging sec fonts.xml to android font_fallback.xml..."
+				$patchFilePath = Join-Path $secFontsPath "fonts.xml"
+				$mainXmlFallback = [xml](Get-Content $fontFallbackXmlPath)
+				$patchXml = [xml](Get-Content $patchFilePath)
+				Merge-XmlContents -mainXml $mainXmlFallback -patchXml $patchXml
+				$mainXmlFallback.Save($fontFallbackXmlPath)
+				Write-Host "... done!"
+			} else {
+				Write-Host "font_fallback.xml not found in android studio fonts directory!"
+			}
         } else {
             Write-Host "plugins\design-tools\resources\layoutlib\data\fonts not found in $dir, skipping..."
         }
+		Write-Host "`n`n`n"
     }
 
     if (-not $found) {
         Write-Host "Error: No plugins\design-tools\resources\layoutlib\data\fonts directory found in any subfolder of ProgramFiles\Android." -ForegroundColor Red
     } else {
         Write-Host "Done!"
+		Write-Host "Restart android studio to take effect."
     }
 
     Pause
@@ -90,12 +116,13 @@ function Run-RestoreStockFonts {
     Get-ChildItem -Directory | ForEach-Object {
         $dir = $_.FullName
         if (Test-Path "$dir\plugins\design-tools\resources\layoutlib\data\fonts_backup") {
-            Write-Host "Restoring original fonts from backup..."
+            Write-Host "Restoring original fonts for $dir from backup..."
             Remove-Item -Path "$dir\plugins\design-tools\resources\layoutlib\data\fonts" -Recurse -Force
             Rename-Item "$dir\plugins\design-tools\resources\layoutlib\data\fonts_backup" fonts
             Write-Host "Done!"
+			Write-Host ""
         } else {
-            Write-Host "Error: fonts_backup folder not found!"
+            Write-Host "Error: fonts_backup folder not found for $dir!"
         }
     }
 
